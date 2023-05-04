087cf6e8780181f2fd9b3e3d4adcf8be4bb42fe0
Aitor Ortega <61882323+aitorortega03@users.noreply.github.com>
2023-05-04 22:19:58 +0200

    finished js

--- script.js
+++ script.js
@@ -5,13 +5,46 @@ const TEMPERATURE_TYPES = Object.freeze({
 })
 
 const buttonConvert = document.getElementById('convert');
-const tempFromValue = document.getElementById('temp-from');
-const tempToValue = document.getElementById('temp-to');
-const tempToConvertString = document.getElementById('temp-input').value.replace(',', '.');
-const tempToConvert = Number(tempToConvertString);
 
-buttonConvert.addEventListener('click', () =>{
+buttonConvert.addEventListener('click', () => {
+
+    const tempFromValue = document.getElementById("temp-from").value;
+    const tempToValue = document.getElementById("temp-to").value;
+    const tempToConvertString = document
+      .getElementById("temp-input")
+      .value.replace(",", ".");
+    const tempToConvert = Number(tempToConvertString);
+
+    let tempConverted = 0;
     
-    
+    if (tempFromValue === TEMPERATURE_TYPES.CELSIUS && tempToValue === TEMPERATURE_TYPES.FAHRENHEIT) {
+        tempConverted = (tempToConvert * 9 / 5) + 32;
+    }
+
+    if (tempFromValue === TEMPERATURE_TYPES.FAHRENHEIT && tempToValue === TEMPERATURE_TYPES.CELSIUS) {
+        tempConverted = (tempToConvert - 32) * 5 / 9;
+    }
+
+    if (tempFromValue === TEMPERATURE_TYPES.CELSIUS && tempToValue === TEMPERATURE_TYPES.KELVIN) {
+        tempConverted = tempToConvert + 273;
+    }
+
+    if (tempFromValue === TEMPERATURE_TYPES.KELVIN && tempToValue === TEMPERATURE_TYPES.CELSIUS) {
+        tempConverted = tempToConvert - 273;
+    }
+
+    if (tempFromValue === TEMPERATURE_TYPES.FAHRENHEIT && tempToValue === TEMPERATURE_TYPES.KELVIN) {
+        tempConverted = (tempToConvert - 32) * 5 / 9 + 273
+    }
+
+    if (tempFromValue === TEMPERATURE_TYPES.KELVIN && tempToValue === TEMPERATURE_TYPES.FAHRENHEIT) {
+        tempConverted = (tempToConvert - 273) * 9 / 5 + 32;
+    }
+
+    if (tempFromValue === tempToValue) {
+        tempConverted = tempToConvert;
+    }
+
+    document.getElementById('result-text').textContent = tempConverted + " " + tempToValue[0].toUpperCase();
 
 });
